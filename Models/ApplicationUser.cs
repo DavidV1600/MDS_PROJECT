4ecbec9e3bd394c8f4b444adc43cbdc8ad7d46f4
AlexGhena <alexandru.ghena-ionescu@s.unibuc.ro>
2024-04-16 18:07:17 +0300

    login implemented

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+
+namespace MDS_PROJECT.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem>? AllRoles { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-using MDS_PROJECT.Data;
+using MDS_PROJECT.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MDS_PROJECT.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +11,23 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+//Serviciile necesare pentru clasa ApplicationUser (pt roluri)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>(
+        );
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//Atasam metoda Initialize din SeedData dupa Build()
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
