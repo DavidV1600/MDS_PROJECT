64678e28489c2cefe6a7278d9c23b8c17758264b
David Voinescu <david.voinescu20030616@gmail.com>
2024-04-30 21:49:49 +0300

    Added basic way of route to closest market

---
+++ Models/SearchResults.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MDS_PROJECT.Models
+{
+    public class SearchResults
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Retailer { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string Quantity { get; set; }
+
+        public string MeasureQuantity { get; set; }
+
+        public string Price { get; set; }
+    }
+}
