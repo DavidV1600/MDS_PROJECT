25a09dcba35ef72576c9338bf36277f0e698e621
David Voinescu <david.voinescu20030616@gmail.com>
2024-05-06 14:32:26 +0300

    product class corrected

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,14 +2,22 @@
 
 namespace MDS_PROJECT.Models
 {
-	public class Product
-	{
-		[Key]
-		public int Id { get; set; }
-		public string Title { get; set; }
-		public string Subtitle { get; set; }
-		public int Price { get; set; }
-		public int Quantity { get; set; }
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
 
-	}
+                // Pass the list of href link texts to the view
+                return View(hrefTexts);
+            }
+            else
+            {
+                // Handle failed HTTP request
+                return Content($"Failed to retrieve the search results page: {searchResponse.StatusCode}");
+            }
+        }
+    }
 }
